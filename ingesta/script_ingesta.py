7f231c05cf5294397974a393f7506d61ab8cb2bb
Gino Fierro <gin.fierro@duocuc.cl>
2026-06-28 18:27:42 -0400

    Adición de prints debug y otros arreglos en las dependencias

--- ingesta/script_ingesta.py
+++ ingesta/script_ingesta.py
@@ -16,6 +16,7 @@ endpoint = 'https://gestioniaapi.onrender.com/enviar-datos'
 load_dotenv(find_dotenv())
 
 apikey = os.getenv("PASSWORD_SUPABASE")
+print(apikey) # DEBUG
 
 headers_api_cuenta = {
     "Schema": "bronze",
